696ab140b2077ac462145e10a56aaf2369e08e58
oluwamaxx <amaechiosita0812@gmail.com>
2026-09-20 00:33:19 +0100

    Fix product images not loading in production

--- kingpinwood/urls.py
+++ kingpinwood/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("", include("store.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpattern =s + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
